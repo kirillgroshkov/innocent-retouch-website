c41b9e3ddf9ef4ecdc65ffb3fb4e63538f8d5315
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2018-02-17 23:42:37 +0100

    data

---
+++ src/data/data.ts
@@ -0,0 +1,26 @@
+export const D = {
+  languages: [],
+  menus: [
+    {
+      id: 'contact',
+      page: '/contact',
+      label: 'contact',
+    },
+    {
+      id: 'fashion-beauty',
+      page: '/',
+      label: 'fashion/beauty',
+    },
+    {
+      id: 'interior',
+      page: '/interior',
+      label: 'interior',
+    },
+    {
+      id: 'still-life',
+      page: '/still-life',
+      label: 'still life2',
+    },
+  ],
+  pages: [],
+}
